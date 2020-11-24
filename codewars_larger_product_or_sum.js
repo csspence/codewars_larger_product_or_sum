a8a489b06a4a5f4b26267c616eda398053fa193e
Crew Spence <crewspence@gCrewSpence.wowway.com>
2020-11-24 06:44:14 -0500

    Wrote working solution.

--- codewars_larger_product_or_sum.js
+++ codewars_larger_product_or_sum.js
@@ -17,6 +17,43 @@ The product of the lowest 3 numbers is 8 x 9 x 10 = 720
 The product of the 3 lowest numbers is higher than the sum of the 3 highest numbers so the function returns "product"
 */
 
-function sumOrProduct(array, n) {
-  // your code here
+const sumOrProduct = (array, n) => {
+  let sorted = array.sort(function(a, b) {
+    return a - b;
+  });
+  let largest = [];
+  let smallest = [];
+  let sum;
+  let product;
+  for(let i = 0; i < sorted.length; i++) {
+    if(i > -1 && i < n) {
+      smallest.push(sorted[i]);
+    }
+    if(i > sorted.length - n - 1) {
+      largest.push(sorted[i]);
+    }
+  }
+  for(let i = 0; i < largest.length; i++) {
+    if(i === 0) {
+      sum = largest[i];
+    } else {
+      sum += largest[i];
+    }
+  }
+  for(let i = 0; i < smallest.length; i++) {
+    if(i === 0) {
+      product = smallest[i];
+    } else {
+      product = product * smallest[i];
+    }
+  }
+
+  if(product > sum) {
+    return 'product';
+  } else if(sum > product) {
+    return 'sum';
+  } else {
+    return 'same';
+  }
+
 }
